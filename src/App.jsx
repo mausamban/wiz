afdd1d039cdaedf1fec901e6640d4f5437268ebd
mausamban <mausamban07@gmail.com>
2024-11-03 07:56:03 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from "./components/header";
+import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Services from "./components/Services";
 import Thought from "./components/Thought";
